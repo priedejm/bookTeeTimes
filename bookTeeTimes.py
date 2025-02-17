436bed9d9636f1edcddd15ccff17c512a617a7bd
jpriede <jpriede@insiderealestate.com>
2025-02-17 17:20:27 -0500

    ready to fetch a single tee time

--- bookTeeTimes.py
+++ bookTeeTimes.py
@@ -113,12 +113,14 @@ def use_selenium_with_cookies(min_time, max_time, players, day, numTeeTimes):
     try:
         print("Opening the login page...")
         driver.get("https://sccharlestonweb.myvscloud.com/webtrac/web/splash.html?InterfaceParameter=WebTrac_Golf")
+        print("fetched")
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
-
+        print("error here")
         # Find and click the login button
         login_button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, "//a[contains(@href, 'login.html')]"))
         )
+        print("no error here")
         login_button.click()
         print("✅ Login button clicked!")
 
